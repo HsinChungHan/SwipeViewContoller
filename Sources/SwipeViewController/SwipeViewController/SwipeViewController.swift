57a0bd05e865b0197a6453dbaaccdcb8f8b3d7bc
HsinChungHan <hooy123456@gapp.nthu.edu.tw>
2020-05-04 14:37:43 +0800

    use tap gesture for handling tap

--- Sources/SwipeViewController/SwipeViewController/SwipeViewController.swift
+++ Sources/SwipeViewController/SwipeViewController/SwipeViewController.swift
@@ -11,23 +11,29 @@ import UIKit
 fileprivate let barDeselectColor = UIColor.init(white: 0, alpha: 0.1)
 fileprivate let barSelectColor = UIColor.white
 
-public protocol SwipeViewControllerDataSource: AnyObject {
-  func swipeViewControllerAllViewControllers(_ swipingPhotosController: SwipeViewController) -> [UIViewController]
-}
-
 public class SwipeViewController: UIViewController {
-  
-  weak public var dataSource: SwipeViewControllerDataSource?
   
   fileprivate lazy var pagingVC = makePagingVC()
   fileprivate lazy var barStackView = makeBarStackView()
-  fileprivate lazy var allViewControllers = makeAllControllers()
+  fileprivate lazy var tapController = makeTapController()
+  fileprivate let viewControllers: [UIViewController]
+  fileprivate let swipeViewControllerModel = SwipeViewControllerModel()
+  
+  public init(viewControllers: [UIViewController]) {
+    self.viewControllers = viewControllers
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
   
   override public func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
     setupLayout()
-    setViewControlersIntoPagingVC(allViewControllers: allViewControllers, pagingVC: pagingVC)
+    setViewControlersIntoPagingVC(allViewControllers: viewControllers, pagingVC: pagingVC)
+    tapController.addTapGesture(in: view)
   }
 }
 
@@ -40,15 +46,11 @@ extension SwipeViewController {
   }
   
   fileprivate func makeBarStackView() -> UIStackView {
-    guard let dataSource = dataSource else {
-      fatalError("🚨 You have to set dataSource for UserDetailViewController")
-    }
-    let countOfControllers = dataSource.swipeViewControllerAllViewControllers(self).count
     let stackView = UIStackView()
     stackView.axis = .horizontal
     stackView.spacing = 4.0
     stackView.distribution = .fillEqually
-    for _ in 0 ... countOfControllers - 1 {
+    for _ in 0 ... (viewControllers.count - 1) {
       let barView = UIView()
       barView.backgroundColor = barDeselectColor
       barView.layer.cornerRadius = 2.0
@@ -59,11 +61,11 @@ extension SwipeViewController {
     return stackView
   }
   
-  fileprivate func makeAllControllers() -> [UIViewController] {
-    guard let dataSource = dataSource else {
-      fatalError("🚨 You have to set dataSource for UserDetailViewController")
-    }
-    return dataSource.swipeViewControllerAllViewControllers(self)
+  fileprivate func makeTapController() -> TapController {
+    let tapController = TapController()
+    tapController.dataSource = self
+    tapController.delegate = self
+    return tapController
   }
   
   fileprivate func setupLayout() {
@@ -81,53 +83,105 @@ extension SwipeViewController {
     }
     pagingVC.setViewControllers([firstVC], direction: .forward, animated: false, completion: nil)
   }
+  
+  fileprivate func setBarColor(_ index: Int) {
+    //讓目前呈現的 VC 對應的 barStackView 的 bar 更改顏色
+    barStackView.arrangedSubviews.forEach{
+      $0.backgroundColor = barDeselectColor
+    }
+    barStackView.arrangedSubviews[index].backgroundColor = barSelectColor
+  }
 }
 
 extension SwipeViewController: UIPageViewControllerDataSource {
   public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
     //得到前一個頁面
-    guard let viewControllerIndex = allViewControllers.firstIndex(of: viewController) else {
-        return nil
-    }
-     
-    let previousIndex = viewControllerIndex - 1
-    let minIndex = 0
-     
-    //如果當前頁面是第一個 VC，向右滑顯示最後一個 VC
-    if previousIndex < minIndex {
-      return allViewControllers.last
+    guard let viewControllerIndex = viewControllers.firstIndex(of: viewController) else {
+      return nil
     }
     
-    return allViewControllers[previousIndex]
+    let previousIndex = viewControllerIndex - 1
+    let minIndex = 0
+    
+    //如果當前頁面是第一個 VC，向右滑顯示最後一個 VC
+    if previousIndex < minIndex {
+      return viewControllers.last
+    }
+    
+    return viewControllers[previousIndex]
   }
   
   public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
     //得到後一個頁面
-    guard let viewControllerIndex = allViewControllers.firstIndex(of: viewController) else {
-        return nil
+    guard let viewControllerIndex = viewControllers.firstIndex(of: viewController) else {
+      return nil
     }
     let nextIndex = viewControllerIndex + 1
-    let maxIndex = allViewControllers.count - 1
-     
+    let maxIndex = viewControllers.count - 1
+    
     //如果當前頁面是最後一個 VC，向左滑顯示第一個 VC
     if nextIndex > maxIndex {
-      return allViewControllers.first
+      return viewControllers.first
     }
     
-    return allViewControllers[nextIndex]
+    return viewControllers[nextIndex]
   }
 }
 
 extension SwipeViewController: UIPageViewControllerDelegate {
+  
   public func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
     let currentVC = pageViewController.viewControllers?.first
-    if let index = allViewControllers.firstIndex(where: { $0 == currentVC }) {
-      //讓目前呈現的 VC 對應的 barStackView 的 bar 更改顏色
-      barStackView.arrangedSubviews.forEach{
-        $0.backgroundColor = barDeselectColor
-      }
-      barStackView.arrangedSubviews[index].backgroundColor = barSelectColor
+    if let index = viewControllers.firstIndex(where: { $0 == currentVC }) {
+      setBarColor(index)
     }
   }
 }
 
+extension SwipeViewController: TapControllerDataSource {
+  func tapControllerWidthOfView(_ tapController: TapController) -> CGFloat {
+    return view.frame.width
+  }
+  
+  func tapControllerGestureLocationX(_ tapController: TapController) -> CGFloat {
+    return swipeViewControllerModel.locationX
+  }
+  
+  
+  func tapControllerIndexOfCurrentController(_ tapController: TapController) -> Int {
+    return swipeViewControllerModel.currentIndex
+  }
+  
+  func tapControllerCountOfControllers(_ tapController: TapController) -> Int {
+    return viewControllers.count
+  }
+}
+
+extension SwipeViewController: TapControllerDelegate {
+  func tapControllerDidTapForward(_ tapController: TapController, nextIndex: Int) {
+    let currentVC = viewControllers[nextIndex]
+    pagingVC.setViewControllers([currentVC], direction: .forward, animated: true) {[weak self] (finished) in
+      guard let self = self else {return}
+      if finished {
+        self.swipeViewControllerModel.set(currentIndex: nextIndex)
+        self.setBarColor(self.swipeViewControllerModel.currentIndex)
+      }
+    }
+  }
+  
+  func tapControllerDidTapBack(_ tapController: TapController, nextIndex: Int) {
+    let currentVC = viewControllers[nextIndex]
+    pagingVC.setViewControllers([currentVC], direction: .reverse , animated: true) {[weak self] (finished) in
+      guard let self = self else {return}
+      if finished {
+        self.swipeViewControllerModel.set(currentIndex: nextIndex)
+        self.setBarColor(self.swipeViewControllerModel.currentIndex)
+      }
+    }
+  }
+  
+  func tapControllerDidTap(_ tapController: TapController, gesture: UITapGestureRecognizer) {
+    let locationX = gesture.location(in: view).x
+    swipeViewControllerModel.set(locationX: locationX)
+  }
+}
